31e78800f527c785d2a25f93e37a1df6a93913c0
mikegee <gee.24@osu.edu>
2011-10-27 12:41:31 -0400

    skip CanCan authorization

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,25 +1,28 @@
 class UserSessionsController < ApplicationController
 
-	skip_before_filter :require_shibboleth
-	before_filter :not_in_production
+  # skip CanCan authorization
+  skip_authorization_check if respond_to?(:skip_authorization_check)
 
-	def new
-		@users = User.all
-	end
+  skip_before_filter :require_shibboleth
+  before_filter :not_in_production
 
-	def create
-		session[:simulate_id] = params[:user_id]
-		redirect_to root_url
-	end
+  def new
+    @users = User.all
+  end
 
-	def destroy
-		session[:simulate_id] = nil
-		redirect_to new_user_session_url, :notice => "Logout successful!"
-	end
+  def create
+    session[:simulate_id] = params[:user_id]
+    redirect_to root_url
+  end
 
-	private
-	def not_in_production
-		redirect_to root_url if Rails.env.production?
-	end
+  def destroy
+    session[:simulate_id] = nil
+    redirect_to new_user_session_url, :notice => "Logout successful!"
+  end
+
+  private
+  def not_in_production
+    redirect_to root_url if Rails.env.production?
+  end
 
 end
