3686bf2323b65a05d4c03316462608d914706747
Mike Butsko <butsko.7@osu.edu>
2013-11-26 12:57:31 -0500

    Deal with case where first_name and last_name are delegated to other classes

--- lib/shibboleth-rails/user_model_additions.rb
+++ lib/shibboleth-rails/user_model_additions.rb
@@ -17,8 +17,9 @@ module Shibboleth::Rails
         # names change due to marriage, etc.
         # update_attribute is a NOOP if not different
         user.update_attribute(:name_n, identity[:name_n])
-        user.update_attribute(:first_name, first_name) if user.respond_to?(:first_name) && first_name.present?
-        user.update_attribute(:last_name, last_name) if user.respond_to?(:last_name) && last_name.present?
+        user.update_attribute(:emplid, identity[:emplid])
+        user.update_attribute(:first_name, first_name) if user.class.columns.map(&:first_name).include?('first_name') && first_name.present?
+        user.update_attribute(:last_name, last_name) if user.class.columns.map(&:last_name).include?('last_name') && last_name.present?
 
         user.update_role(affiliations) if user.respond_to?(:update_role)
         user
