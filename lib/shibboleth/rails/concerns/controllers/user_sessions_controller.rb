2133b2922ca1b0f486090ae5ce728ae3bcd61518
Kyle Decot <decot.7@osu.edu>
2014-01-27 11:30:31 -0500

    Making sure we do main_app.root_url

--- lib/shibboleth/rails/concerns/controllers/user_sessions_controller.rb
+++ lib/shibboleth/rails/concerns/controllers/user_sessions_controller.rb
@@ -22,7 +22,7 @@ module Shibboleth
             def create
               session[:simulate_id] = params[:user_id]
               target = session.delete :target
-              redirect_to target || root_url
+              redirect_to target || main_app.root_url
             end
 
             def destroy
@@ -33,7 +33,7 @@ module Shibboleth
             private
 
             def not_in_production
-              redirect_to root_url if ::Rails.env.in? Shibboleth::Rails::LIVE_ENVS
+              redirect_to main_app.root_url if ::Rails.env.in? Shibboleth::Rails::LIVE_ENVS
             end
 
           end
